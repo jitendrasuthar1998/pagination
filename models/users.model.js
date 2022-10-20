76517d021e23d69d24051f33f7bc4ceb07b4c90f
CrioUser <crio-user@criodo.com>
2022-10-21 00:58:17 +0530

    connected backend application to mongodb

--- models/users.model.js
+++ models/users.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required}
+    name: {type: String, required: true}
 })
 
 module.exports = mongoose.model("User", userSchema); 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose");
+const User = require("./models/users.model");
+
 
 
 // const {users} = require("./users.json");
@@ -8,48 +10,84 @@ const mongoose = require("mongoose");
 
 
 
+const mongoDBUrl = "mongodb://localhost/pagination"
+
+mongoose.connect(mongoDBUrl, () => {
+    console.log("connected to database");
+})
 
 
+const db = mongoose.connection;
 
-app.get("/users", paginatedResult(users), (req, res) => {
+db.once('open', async () => {
+    if (await User.countDocuments().exec() > 0) return;
+
+    Promise.all([
+        User.create({ name: "User 1" }),
+        User.create({ name: "User 2" }),
+        User.create({ name: "User 3" }),
+        User.create({ name: "User 4" }),
+        User.create({ name: "User 5" }),
+        User.create({ name: "User 6" }),
+        User.create({ name: "User 7" }),
+        User.create({ name: "User 8" }),
+        User.create({ name: "User 9" }),
+        User.create({ name: "User 10" }),
+        User.create({ name: "User 11" }),
+        User.create({ name: "User 12" }),
+        User.create({ name: "User 13" }),
+        User.create({ name: "User 14" }),
+        User.create({ name: "User 15" }),
+        User.create({ name: "User 16" }),
+        User.create({ name: "User 17" }),
+        User.create({ name: "User 18" }),
+        User.create({ name: "User 19" }),
+        User.create({ name: "User 20" }),
+    ]).then(() => console.log("added users to database successfully"))
+})
+
+app.get("/users", paginatedResult(User), (req, res) => {
     res.json(res.paginatedResult);
 })
 
-app.get("/posts", paginatedResult(posts), (req, res) => {
-    res.json(res.paginatedResult);
-})
+// app.get("/posts", paginatedResult(posts), (req, res) => {
+//     res.json(res.paginatedResult);
+// })
 
 
 function paginatedResult(model) {
-    return (req, res, next) => {
+    return async(req, res, next) => {
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
-    
+
         const startIndex = (page - 1) * limit;
         const endIndex = page * limit;
-    
+
         // console.log("startIndex is == ", startIndex);
         // console.log("endIndex is == ", endIndex);
-    
+
         const result = {};
-        if (endIndex < model.length) {
+        if (endIndex < await model.countDocuments().exec()) {
             result.next = {
                 page: page + 1,
                 limit: limit
             }
         }
-    
+
         if (startIndex > 0) {
             result.previous = {
                 page: page - 1,
                 limit: limit
             }
         }
-
-        result.data = model.slice(startIndex, endIndex)
-    
-        res.paginatedResult = result
-        next();
+        console.log("result ", result)
+        try {
+            result.data = await model.find().limit(limit).skip(startIndex).exec()
+            res.paginatedResult = result
+            next();
+        } catch (e) {
+            res.status(500).json({ message: e.message });
+        }
     }
 }
 
